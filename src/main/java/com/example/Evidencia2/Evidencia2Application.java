cf556df3f4ded3c2b250c331d9a7320a7a8ed8f7
luiskain <luiskainlozan@gmail.com>
2023-11-22 14:42:20 -0600

    first commit

---
+++ src/main/java/com/example/Evidencia2/Evidencia2Application.java
@@ -0,0 +1,13 @@
+package com.example.Evidencia2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evidencia2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Evidencia2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Evidencia2/Evidencia2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Evidencia2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evidencia2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
